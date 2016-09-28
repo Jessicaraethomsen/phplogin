97e83f3a4a8cbc302507fc51eb23d34b08cadc51
Jessicaraethomsen <jessicathomsen@Jessicas-MacBook-Pro-2.local>
2016-09-28 10:00:02 +0200

    Making the SIGN IN Form & Connect
    1. Make a Form for the sign in
    2. connect it to the login.php

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
 </head>
 
 <body>
+
+<h4> SIGN IN</h4>
+<form action="login.php" method="POST" > 
+<input type="text" name="uid" placeholder="Username" required><br>
+<input type="Password" name="pwd" placeholder="Password" required><br>
+<button type="submit"> SIGN IN</button>
+</form>
+
+
 <!--Making a form to develop the SIGN UP process-->
 <h4> SIGN UP </h4>
 <form action="signup.php" method="POST"> 
@@ -16,7 +25,7 @@
 <button type="submit"> SIGN UP</button>
 </form>
 
-<br><br>
+<br><br> 
 
 
 
