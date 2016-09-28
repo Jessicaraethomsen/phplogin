cb7348983db696a88b0a6a010a619fd2572458f0
Jessicaraethomsen <jessicathomsen@Jessicas-MacBook-Pro-2.local>
2016-09-28 09:34:39 +0200

    Connecting the signup form to INSERT INTO table
     1. Making Variables & posting the variables to each of this sections
    2. Then inserting into the table user with the values of the variables.
    3. Returning back to the index page after the it is sign up

---
+++ signup.php
@@ -0,0 +1,16 @@
+<?php 
+include 'dbh.php';
+
+$first = $_POST['first'];
+$last = $_POST['last'];
+$uid = $_POST['uid'];
+$pwd = $_POST['pwd'];
+// posting the variables to each of this sections
+
+$sql = "INSERT INTO user (first, last, uid, pwd) VALUES ('$first', '$last', '$uid', '$pwd')";
+// then inserting into the table user with the values of the variables.
+
+$result = mysqli_query($conn, $sql);
+
+header("Location: index.php");
+?>
