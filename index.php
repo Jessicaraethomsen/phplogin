ddba9347108a6ac8ef0fb5b612966664d12ddb21
Jessicaraethomsen <jessicathomsen@Jessicas-MacBook-Pro-2.local>
2016-09-28 09:20:49 +0200

    Making a form (SIGNUP)
    Making a form to develop the SIGN UP process.

--- index.php
+++ index.php
@@ -6,5 +6,15 @@
 </head>
 
 <body>
+<!--Making a form to develop the SIGN UP process-->
+<h4> SIGN IN</h4>
+<form action="login.php" method="POST" > 
+<input type="text" name="UID" placeholder="Username" required><br>
+<input type="Password" name="PWD1" placeholder="Password" required><br>
+<button type="submit"> SIGN IN</button>
+</form>
+
+
+
 </body>
 </html>
