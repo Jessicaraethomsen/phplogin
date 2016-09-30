831bd771f2b2433462ff1a1a2033d0163d58689d
Jessicaraethomsen <jessicathomsen@Jessicas-MacBook-Pro-2.local>
2016-09-30 15:10:16 +0200

    changing the database to upload live!

--- dbh.php
+++ dbh.php
@@ -1,7 +1,7 @@
 <?php
 //makes connection to the DB (LOCAL) need to fix later to my database live
-//$conn = mysqli_connect("jessicaraethomsen.dk.mysql", "jessicaraethoms", "vKeJJ2C6", "jessicaraethoms");
-$conn = mysqli_connect("localhost", "root", "", "user");
+$conn = mysqli_connect("jessicaraethomsen.dk.mysql", "jessicaraethoms", "vKeJJ2C6", "jessicaraethoms");
+//$conn = mysqli_connect("localhost", "root", "", "user");
 
 if (!$conn) {
 	die("Connection failed:".mysqli_connect_error());
